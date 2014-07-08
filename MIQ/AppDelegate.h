11efc70038012a653d54b90fb8a5f2b824394f59
Luis Kentzler <luis@kentzler.me>
2014-07-08 11:57:48 -0500

    Initial Commit

---
+++ MIQ/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MIQ
+//
+//  Created by Work Station on 7/8/14.
+//  Copyright (c) 2014 GlobalPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

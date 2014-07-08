e841da5b60335ded4232d356bad240f4df6f9ec1
Luis Kentzler <luis@kentzler.me>
2014-07-08 11:26:31 -0500

    Initial Commit

---
+++ TEST/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TEST
+//
+//  Created by Luis Kentzler on 7/8/14.
+//  Copyright (c) 2014 GlobalPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TEST/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TEST
+//
+//  Created by Luis Kentzler on 7/8/14.
+//  Copyright (c) 2014 GlobalPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

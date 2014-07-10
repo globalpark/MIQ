4cf9d8f4eacac05392365f2a67820d247e6aa2b7
Luis Kentzler <luis@kentzler.me>
2014-07-09 19:21:32 -0500

    Added Graphic Layout to the Events section. (Favourites Heart missing in the last window (Event Detail)

---
+++ MIQ/EventosListaTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventosListaTableViewController.h
+//  MIQ
+//
+//  Created by Work Station on 7/9/14.
+//  Copyright (c) 2014 GlobalPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventosListaTableViewController : UITableViewController
+
+@end
---
+++ MIQ/HomeViewController.m
@@ -0,0 +1,59 @@
+//
+//  HomeViewController.m
+//  MIQ
+//
+//  Created by Work Station on 7/9/14.
+//  Copyright (c) 2014 GlobalPark. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    
+    //Change status bar to Light theme
+    [self setNeedsStatusBarAppearanceUpdate];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - View Setup
+
+//Change status bar to Light theme
+-(UIStatusBarStyle)preferredStatusBarStyle{
+    return UIStatusBarStyleLightContent;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
